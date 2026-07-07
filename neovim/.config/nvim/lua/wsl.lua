c3e84e3a418d2bca9125eb145dcc0b8666f65ebb
Sophie Eccles <sae@eccles.dev>
2026-07-07 14:47:58 -0400

    nvim: use OSC 52 for copy on windows terminal

--- neovim/.config/nvim/lua/wsl.lua
+++ neovim/.config/nvim/lua/wsl.lua
@@ -1,16 +1,16 @@
-local clip = "/mnt/c/Windows/System32/clip.exe"
 local pwsh = "/mnt/c/Windows/System32/WindowsPowershell/v1.0/powershell.exe"
 local pwsh_args = ' -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))'
 
 vim.g.clipboard = {
   name = "WslClipboard",
   copy = {
-    ["+"] = clip,
-    ["*"] = clip,
+    -- for copying, windows terminal supports OSC 52
+    ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
   },
   paste = {
+    -- for pasting, windows terminal does not support OSC 52
     ["+"] = pwsh .. pwsh_args,
     ["*"] = pwsh .. pwsh_args,
   },
-  cache_enabled = false,
 }
